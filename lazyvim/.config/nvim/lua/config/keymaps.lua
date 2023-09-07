ba2ff660ff8df4910e5e301bc775aa57244048af
Lintao <lintao.amons@outlook.com>
2023-09-07 23:25:44 +0800

    Update

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.del("n", "<leader>l", {})
+vim.keymap.del({ "n", "i", "v" }, "<A-j>", {})
+vim.keymap.del({ "n", "i", "v" }, "<A-k>", {})
 
 local command_keymappings = {
   -- HACK: Debug
